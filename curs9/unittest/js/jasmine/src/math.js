544c6162eb9a5a68e238644cb99fb56f83acc358
Adela Mag <adela_mag@yahoo.com>
2025-01-30 20:44:05 +0200

    Curs9

---
+++ curs9/unittest/js/jasmine/src/math.js
@@ -0,0 +1,12 @@
+function helloWorld(){
+  return "Hello World";
+}
+
+function sum(a,b) {
+  // return ((a+b)*100)/100;
+  a = a*100;
+  b = b*100;
+  let s = a+b;
+  s = s/100;
+  return Number(s.toFixed(2));
+}
